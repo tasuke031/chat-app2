f313a7421841149384ec8a267801de5c6e57fa50
tasuke031 <yosuket031@gmail.com>
2020-08-22 17:07:51 +0900

    messagesテーブルの作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,8 +7,8 @@ class RoomsController < ApplicationController
   end
 
   def create
-    @room = Room.new(room_params)
-    if @room.save
+    room = Room.new(room_params)
+    if room.save
       redirect_to root_path
     else
       render :new
